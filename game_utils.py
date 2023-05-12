7b0e19633105d59a5baf2ab09d369cdbc0257e08
Emre Iper <mereiper789@gmail.com>
2023-05-13 01:05:03 +0300

    End game and catch count added

--- game_utils.py
+++ game_utils.py
@@ -8,7 +8,6 @@ def setup_turtle(turtle_game):
     turtle_game.color("green")
     turtle_game.shape("turtle")
     turtle_game.turtlesize(3)
-    turtle_game.left(90)
 
 
 def random_turtle_loc(screen, turtle_game):
@@ -19,8 +18,25 @@ def random_turtle_loc(screen, turtle_game):
     turtle_game.showturtle()
 
 
-def time_tex_loc(screen, turtle_time, duration=0):
+def time_text_loc(screen, turtle_time, duration=0):
     turtle_time.hideturtle()
     turtle_time.penup()
     turtle_time.goto(-40, int(screen.window_height() // 3 + 70))
     turtle_time.write(f"Time: {duration}", font=("Verdana", 20, "bold"))
+
+
+def point_text_loc(screen, turtle_point, count=0):
+    turtle_point.hideturtle()
+    turtle_point.penup()
+    turtle_point.goto(-40, int(screen.window_height() // 3 + 40))
+    turtle_point.clear()
+    turtle_point.write(f"Point: {count}", font=("Verdana", 20, "bold"))
+
+
+def end_game(turtle_end):
+    turtle_end.clear()
+    turtle_end.hideturtle()
+    turtle_end.penup()
+    turtle_end.color("black")
+    turtle_end.goto(0, 100)
+    turtle_end.write("Game Over", font=("Verdana", 50, "bold"), align="center")
--- main.py
+++ main.py
@@ -10,13 +10,15 @@ screen.bgcolor("light blue")
 
 turtle_game = turtle.Turtle()
 turtle_time = turtle.Turtle()
+turtle_point = turtle.Turtle()
+turtle_point.hideturtle()
 
 game_utils.setup_turtle(turtle_game=turtle_game)
 
 
 def turtle_timer():
     if duration > 0:
-        game_utils.random_turtle_loc(screen, turtle_game)
+        game_utils.random_turtle_loc(screen=screen, turtle_game=turtle_game)
         screen.ontimer(turtle_timer, t=500)
 
 
@@ -25,8 +27,19 @@ def game_timer():
     if duration > 0:
         duration -= 1
         turtle_time.clear()
-        game_utils.time_tex_loc(screen=screen, turtle_time=turtle_time, duration=duration)
-        screen.ontimer(game_timer, 1000)
+        game_utils.time_text_loc(screen=screen, turtle_time=turtle_time, duration=duration)
+        turtle_game.onclick(turtle_count)
+        screen.ontimer(game_timer, t=1000)
+    else:
+        game_utils.end_game(turtle_end=turtle_game)
+        turtle_game.hideturtle()
+
+
+def turtle_count(x, y):
+    turtle_point.clear()
+    global count
+    count += 1
+    game_utils.point_text_loc(screen=screen, turtle_point=turtle_point, count=count)
 
 
 turtle_timer()
